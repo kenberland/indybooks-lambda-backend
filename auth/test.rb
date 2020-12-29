7955478bcf9e477b1f2965b03d19f6948465d2f3
Justin <justin@soonahn.ca>
2020-12-29 00:42:57 -0800

    cors sux

--- auth/test.rb
+++ auth/test.rb
@@ -2,5 +2,11 @@ require 'json'
 
 def test_handler(event:, context:)
     # TODO implement
-    { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
+
+  headers_list = {
+    "Access-Control-Allow-Origin" => "*",
+    "Indybooks-git-commit-sha" => $my_git_commit_sha
+  }
+
+  { statusCode: 200, headers: headers_list, body: JSON.generate('Hello from Lambda!') }
 end
