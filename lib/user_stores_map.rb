7d9524792649e321352dc78a51f00d1fb6fc331d
Justin <justin@soonahn.ca>
2021-01-06 23:36:38 -0800

    getting cognito username

--- lib/user_stores_map.rb
+++ lib/user_stores_map.rb
@@ -1,5 +1,5 @@
 # TODO: this will be a DDB lookup and maybe a Redis cached item
 
 $USER_STORES_MAP = {
-  'indybooks' => '5fa6f746-27b6-55b6-9c7d-193fdee21468'
+  'indybooks' => {uuid: '5fa6f746-27b6-55b6-9c7d-193fdee21468', name: "Moe's Books"}
 }
